e08969f2b0c1370c00579c0e85416a4e08250ba2
Namit Nayak <nnayak@sapient.com>
2016-08-22 09:08:04 -0400

    Updated travis build sequence Added package version from package.json

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var Pretty = require('bunyan-pretty-stream');
 var Program = require('commander');
 var fs = require('fs-extra-promise');
 var Walk = require('walk');
+var pack = require('./package.json');
 
 var log = Bunyan.createLogger({
 	name: 'organize-images',
@@ -18,7 +19,7 @@ var options = {
 };
 
 Program
-  .version('0.0.5')
+  .version(pack.version)
   .option('-S, --source <path>', 'source directory of the files')
   .option('-D, --destination <path>',
 		'destination directory of the organized files')
@@ -34,9 +35,7 @@ if (typeof Program.source === 'undefined' ||
 }
 
 Program.destination = Program.destination.replace(/\/?$/, '/');
-
 var walker = Walk.walk(Program.source, options);
-
 walker.on("file", function(root, stat, next) {
 	var sourcePath = root + '/' + stat.name;
 	var targetPath = Program.destination +
@@ -45,13 +44,10 @@ walker.on("file", function(root, stat, next) {
 		stat.birthtime.getDate() +
 		'/' +
 		stat.name;
-
 	fs.copyAsync(sourcePath, targetPath).then(function() {
-
 	}).catch(function(error) {
 		log.error(error);
 	});
-
 	next();
 });
 
